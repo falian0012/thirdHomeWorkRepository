773cb6d20702c6927d2a4c6fe5768d66cbf1ca6b
falian0012 <falian0012@gmail.com>
2020-11-16 23:00:12 +0300

    Задание 5

---
+++ Task_5/task_5.py
@@ -0,0 +1,16 @@
+break_point = True
+
+while break_point:
+    list = input().split(" ")
+    temp_value = 0
+    for item in list:
+        if item == "#":
+            break_point = False
+            break
+        temp_value += int(item)
+    if break_point:
+        print(temp_value)
+
+print("The END!")
+
+
